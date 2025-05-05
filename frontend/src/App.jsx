0464786859ce60ca085b33b932900d76e87459a1
MadhuMithr <madh22082.cs@rmkec.ac.in>
2025-05-05 16:00:03 +0530

    react project created and login page added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+// import Login from "./Login.jsx";
+// import React from "react";
+// function App() {
+//   return(
+//       <Login/>
+    
+//   );
+// }
+
+// export default App
+import React from "react";
+import Login from "./Login";
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Login.jsx
@@ -0,0 +1,27 @@
+import React,{useState} from "react";
+import "./Login.css";
+
+const Login= ()=>{
+    const [email,setEmail]=useState('');
+    const [password,setPassword]=useState('');
+
+    return(
+        <div className="login-container">
+            <h2>LOGIN</h2>
+            <form >
+                <input type="email"
+                placeholder="E-mail"
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)} required/>
+                <input type="password"
+                placeholder="Enter password"
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)}
+                required/>
+                <button type="submit">Login</button>
+            </form>
+
+        </div>
+    );
+}
+export default Login;
