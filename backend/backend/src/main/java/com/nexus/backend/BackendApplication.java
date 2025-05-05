6da948dc258a72f5b1a7103288c685e1bc3c6e37
MadhuMithr <madh22082.cs@rmkec.ac.in>
2025-05-05 19:36:12 +0530

    database connected

--- backend/backend/src/main/java/com/nexus/backend/BackendApplication.java
+++ backend/backend/src/main/java/com/nexus/backend/BackendApplication.java
@@ -2,7 +2,7 @@ package com.nexus.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//asdfghjkoredfghhgv
 @SpringBootApplication
 public class BackendApplication {
 
